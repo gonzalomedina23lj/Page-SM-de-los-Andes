04de86410d254623ebaa809c0c0016b216d18bce
Dissident <gonzalomedina23lj@gmail.com>
2022-02-04 18:28:04 -0300

    04/02/2022_2

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,4 @@
-const iconElement = document.querySelector(".iconweather");
+/* const iconElement = document.querySelector(".iconweather");
 const tempElement = document.querySelector(".temperature p");
 const windElement = document.querySelector(".windspeed")
 const humElement = document.querySelector(".humvalue")
@@ -7,17 +7,16 @@ iconElement.innerHTML =
 tempElement.innerHTML =
 windElement.innerHTML =
 humElement.innerHTML = 
-
+ */
 
 
 
 fetch('https://api.openweathermap.org/data/2.5/weather?id=3836951&appid=6166d72636703fa3f3b3a0c58d96009c&units=metric')
     .then(response => response.json())
     .then(data => {
-        $("#temp").text(data.main.temp.toFixed(0) + " °C");
-        $("#humidity").text(data.main.humidity+"%")
-        $("#wind").text(data.wind.speed+"km/h")
-        $("weathericon")data
+        $("#temp").text(data.main.temp.toFixed(0) + "°C");
+        $("#humidity").text(data.main.humidity+"%");
+        $("#wind").text(data.wind.speed+"km/h");
         
 
         
