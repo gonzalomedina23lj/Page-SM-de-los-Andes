20d990528f2510fcae39ccf54620b6831353a695
Dissident20 <gonzalomedina23lj@gmail.com>
2023-02-10 18:04:18 -0300

    widget icons edit, optimization page | 10/02/2023_6

--- js/weather.js
+++ js/weather.js
@@ -9,7 +9,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?id=3836951&appid=6166d726
         $("#wind").text(data.wind.speed+"km/h");
         
         const icon = data.weather[0].icon;
-        iconWeather.innerHTML = `<img src='icons/${icon}.png'></img>`
+        iconWeather.innerHTML = `<img src='icons/${icon}.png' alt="icon"></img>`
         $("iconweater").text(data.weather[0].icon);
         
 
